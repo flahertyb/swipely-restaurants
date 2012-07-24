0a13ad97f9d335f1e01b13b426f5fdba7cb643f7
flahertyb <bartholomew.flaheryt@gmail.com>
2012-07-24 16:35:55 -0400

    wrote script for viewing features

--- js/script.js
+++ js/script.js
@@ -1,25 +1,49 @@
+
+var features = function() {
+
+	// takes the feature button (jquery object) that was clicked, 
+	// highlights it, and deselects the other two feature buttons
+	var labelHighlight = function(label){
+
+		var otherlabels = $('.feature-labels').children().not(label);
+
+		$(otherlabels).css('backgroundPosition', '0 0');
+		$(otherlabels).addClass('feature-hover');
+
+		$(label).removeClass('feature-hover');
+		$(label).css('backgroundPosition', '0 -66px');
+	}
+
+	// displays the corresponding picture in the feature-screenshots div
+	var switchContent = function(label){
+
+		var name = $(label).text();
+		console.log(name);
+		switch (name) {
+			case "Analytics":
+				$('div.feature-screenshots').css("backgroundImage", "url('http://www.placekitten.com/400/254')");
+				break;
+			case "Loyalty":
+				$('div.feature-screenshots').css("backgroundImage", "url('http://www.placekitten.com/400/256')");
+				break;
+			case "Marketing":
+				$('div.feature-screenshots').css("backgroundImage", "url('http://www.placekitten.com/400/253')");		
+				break;
+			default:
+				break;
+		}
+	}
+
+	// calls previous functions on click event
+	$('.feature-labels li').click(function(){ 
+			labelHighlight(this); 
+			switchContent(this);
+		});
+
+}
+
 $(document).ready(function(){
-
-	$("#submit").click(function(e){
-
-
-		/*		
-		$(".error").hide();
-		var error = false;
-		var validemail = /^([\w-\.]+@[\w-]+\.[\w]{2,4})?$/;
-	    var emailvalue = $("#email").val();
-		*/
-	    $.ajax({
-	    	type : "POST",
-	    	url : "https://www.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8",
-	    	data : $("#get-lead").serialize(),
-	    	success : function (data) {
-	    			$("#submit").hide();
-	    	}
-
-
-	    });
-	    return false;
-	});
-
+	features();
 });
+
+
