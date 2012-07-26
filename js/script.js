859a8513fd62f383afd618f04ca0cb9f87308296
flahertyb <bartholomew.flaheryt@gmail.com>
2012-07-26 01:19:26 -0400

    added form and made cosmetic adjustments

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/* FEATURE BUTTONS (Analytics, Marketing, Loyalty) */
 var features = function() {
 
 	// takes the feature button (jquery object) that was clicked, 
@@ -47,6 +47,7 @@ var features = function() {
 		});
 }
 
+/* TESTIMONIALS QUOTE/CONTENT BUTTONS */
 var testimonials = function() {
 
 	var duration = 200;
@@ -86,9 +87,85 @@ var testimonials = function() {
 	});
 }
 
+/* FORM VALIDATION AND SUBMISSION - based on Sam Dunn's example at buildinternet.com */
+var webToLead = function() {
+
+	$("#send-form-data").click(function(e){
+
+	    // if user hits submit, existing error spans disappear
+	    $(".error").hide(); 
+
+	    // no errors to start
+	    var error = false;
+	    var validemail = /^([\w-\.]+@[\w-]+\.[\w]{2,4})?$/;
+	    var emailValue = $("#lead-email").val();
+	    var businessValue = $("#lead-business").val();
+
+
+
+	    // if the fields are blank
+	    if ((emailValue == "") || (emailValue == null)){
+		$("#lead-email").after('<p class="error">You forgot to write your email!</p>');
+		error = true;
+	    }
+	    if ((businessValue == "") || (businessValue == null) || ($.trim(businessValue) == 'What is your restaurant called?')){
+		$("#lead-business").after('<p class="error">You forgot to write the name of your organization!"</p>');
+		}
+
+	    // if the email field is invalid (fails regexp test)
+	    if (!validemail.test(emailValue)) {
+		$("#lead-email").after('<p class="error">Your email doesn\'t seem valid, are you sure there are no typos?</p>');
+		error = true;
+	    }
+
+	    // if there are no errors 
+	    if (!error) {
+	    	console.log('hi');
+			$.ajax({
+		    	type : "POST",
+		    	url : "https://www.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8",
+		    	data : $("#lead-capture").serialize(),
+		    	success : function (data) {
+		    			$("#lead-capture").slideUp(500);
+		    	}
+	    	});
+		}
+	    return false;
+    });
+}
+	
+/* STYLE THE FORM */
+
+var formStyling  = function(){
+
+	$('input[type="text"], textarea').addClass("idleField");
+	$('input[type="text"], textarea').focus(function() {
+		$(this).removeClass("idleField").addClass("focusField");
+		if (this.value === this.defaultValue){
+		    this.value = '';
+		}
+		if(this.value !== this.defaultValue){
+		    this.select();
+		}
+	    });
+	$('input[type="text"], textarea').blur(function() {
+		$(this).removeClass("focusField").addClass("idleField");
+		if ($.trim(this.value) === ''){
+		    this.value = this.defaultValue;
+		}
+	});
+}
+
+
+
+
+
+
 $(document).ready(function(){
 	features();
 	testimonials();
+	webToLead();
+	formStyling();
 });
 
 
